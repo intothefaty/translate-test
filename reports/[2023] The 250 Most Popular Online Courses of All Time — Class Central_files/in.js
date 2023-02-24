a3c38eed5588c57e9cd9fe06d27b5e6c8034ab3e
intothefaty <fatih98bay@hotmail.com>
2023-02-24 06:49:43 +0300

    all

---
+++ reports/[2023] The 250 Most Popular Online Courses of All Time — Class Central_files/in.js
@@ -0,0 +1,8 @@
+// (☞ﾟ∀ﾟ)☞ static32
+if( window._cgen ) {
+_cgen.set_cookie('_jsuid', '1048025633');
+_cgen.advanced();
+_cgen.ping_start();
+_cgen.set_cookie('_heatmaps_g2g_100717247', 'yes', 600 );
+_heatmaps_g2g_100717247='yes';
+}
