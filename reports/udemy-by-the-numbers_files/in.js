b066785d11c135a1c72afc4f1e3fea4fb5212608
intothefaty <fatih98bay@hotmail.com>
2023-02-24 07:23:45 +0300

    10759

---
+++ reports/udemy-by-the-numbers_files/in.js
@@ -0,0 +1,10 @@
+// (☞ﾟ∀ﾟ)☞ static34
+if( window._cgen ) {
+_cgen.set_cookie('_jsuid', '1048025633');
+_cgen.advanced();
+_cgen.ping_start();
+_cgen.set_cookie('_heatmaps_g2g_100717247', 'no', 600 );
+_heatmaps_g2g_100717247='no';
+_cgen.set_cookie('_first_pageview', 1, 600 );
+_cgen.set_cookie('_referrer_og', 'http://127.0.0.1/', 30*86400 );
+}
